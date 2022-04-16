cf706dcc4b3e27a2b6c5bfce8c8d02c0fea28917
JustinLaurenso <justin.laurenso166@gmail.com>
2022-04-17 06:44:15 +0700

    Push Movie Apps Frontend and Backend

---
+++ backend/services/Favorite.js
@@ -0,0 +1,30 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+const checkFavorite = async(data_rating) => {
+    const rows = await db.query(`SELECT * FROM favorites WHERE user_id='${data_rating.user_id}' AND movie_id='${data_rating.movie_id}'`)
+    const check = helper.emptyOrRows(rows)
+
+    return check;
+}
+
+const addFavorite = async(data_favorite) => {
+    const rows = await db.query("INSERT INTO favorites (user_id, movie_id) VALUES ('" + data_favorite.user_id + "' ,'" + data_favorite.movie_id + "')");
+    const check = helper.emptyOrRows(rows);
+
+    return check;
+}
+
+const removeFavorite = async(data_rating) => {
+    const rows = await db.query(`DELETE FROM favorites WHERE user_id='${data_rating.user_id}' AND movie_id='${data_rating.movie_id}'`);
+    const check = helper.emptyOrRows(rows)
+
+    return check
+}
+
+module.exports = {
+    checkFavorite,
+    removeFavorite,
+    addFavorite,
+}
